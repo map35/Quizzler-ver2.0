789ee561fb81e8b66f4f907ddfdc70e2b37d7fec
Mohammad Agung <diditkazama@gmail.com>
2020-01-10 10:01:43 +0700

    tambah variabel, konek ui ke viewcontroller, tambah fungsi updateUI dan cekAnswer

---
+++ Quizzler-ver2.0/Quiz.swift
@@ -0,0 +1,13 @@
+//
+//  Quiz.swift
+//  Quizzler-iOS13
+//
+//  Created by Mohammad Agung on 10/01/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Quiz {
+    
+}
--- Quizzler-ver2.0/ViewController.swift
+++ Quizzler-ver2.0/ViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var labelPertanyaan: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let questionArray = [["2 + 2 = 4", "True"], ["3 * 4 = 12", "True"], ["9 / 3 = 0", "False"]]
+    var questionNumber = 0
+    var userAnswer = ""
+    var correctAnswer = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        updateUI()
     }
 
-
+    @IBAction func answerPressed(_ sender: UIButton) {
+        userAnswer = sender.currentTitle!
+        correctAnswer = questionArray[questionNumber][1]
+        
+        if questionNumber+1 < questionArray.count {
+//            updateUI()
+            
+            questionNumber += 1
+        } else {
+//            updateUI()
+            
+            questionNumber = 0
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        labelPertanyaan.text = questionArray[questionNumber][0]
+        
+        cekAnswer()
+    }
+    
+    func cekAnswer() {
+        if correctAnswer == "True" {
+            trueButton.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            falseButton.backgroundColor = #colorLiteral(red: 0.7516429424, green: 0, blue: 0, alpha: 1)
+            
+//            print("yeay")
+        } else if correctAnswer == "False" {
+            trueButton.backgroundColor = #colorLiteral(red: 0.7516429424, green: 0, blue: 0, alpha: 1)
+            falseButton.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            
+//            print("sad")
+        }
+        
+//        print(questionArray[questionNumber][0])
+//        print(correctAnswer)
+    }
 }
 
